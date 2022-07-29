e93b125c7a66781fabcebc410e292d300843ed82
Phnix <76911907+MrPhnix@users.noreply.github.com>
2022-07-29 21:45:14 +0200

    feat: started to implement the json to C# converter

--- scripts/game/CharacterManager.cs
+++ scripts/game/CharacterManager.cs
@@ -2,11 +2,30 @@ using Godot;
 
 namespace Com.LaytonCommunity.RavensCurse
 {
-	public abstract class CharacterManager : Node
+	public enum Position { Left, Right }
+	public enum Type { Base, ArmsCrossed, ArmsDown, ArmsUp, HoldingHat, PointsUp }
+	public enum Emotion { None, Angry, Happy, VeryHappy, Pensive, Sad, Surpised, Thinking }
+
+	public class Dialogue
+	{
+		string sceneName;
+		string csvKey;
+	}
+
+	public class CharacterJson
+	{
+		string name;
+		Type type;
+		Emotion emotion;
+		Position position;
+		Dialogue dialogue;
+	}
+	
+	public class CharacterManager : Node
 	{
 		public override void _Ready()
 		{
-
+			
 		}
 
 		protected override void Dispose(bool disposing)
